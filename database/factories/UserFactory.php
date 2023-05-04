b584254c491d5c95fe4ba35daef34bc901df81a7
Matheus <tavares.matheus.sp@gmail.com>
2023-05-04 19:41:04 -0300

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $firstName = fake()->firstName();
+        $lastName = fake()->lastName();
+        $safeEmailDomain = fake()->safeEmailDomain();
+        return [
+            'name' => $firstName . ' ' . $lastName,
+            'email' => strtolower($firstName) . '.' . strtolower($lastName) . '@' . $safeEmailDomain,
+            'email_verified_at' => now(),
+            'password' => password_hash(fake()->password(), PASSWORD_BCRYPT),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
